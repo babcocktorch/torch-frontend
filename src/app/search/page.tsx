0ef28ebd4bc565a0fb9a6d1b36f919286ef9bb7f
Victor Ibironke <victoribironke2019@gmail.com>
2025-10-31 13:32:07 +0100

    Add metadata for SEO optimization on search page; include Open Graph and Twitter card details

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,4 +1,29 @@
 import { Input } from "@/components/ui/input";
+import { BASE_URL, IMAGES } from "@/lib/constants";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Search the Torch — The Babcock Torch",
+  metadataBase: new URL(BASE_URL),
+  description: "Search the Torch to find articles, stories, and more.",
+  alternates: {
+    canonical: BASE_URL,
+  },
+  openGraph: {
+    images: BASE_URL + IMAGES.logos.logo.src,
+    url: BASE_URL,
+    title: "Search the Torch — The Babcock Torch",
+    description: "Search the Torch to find articles, stories, and more.",
+    type: "website",
+    siteName: "The Babcock Torch",
+  },
+  twitter: {
+    title: "Search the Torch — The Babcock Torch",
+    description: "Search the Torch to find articles, stories, and more.",
+    images: BASE_URL + IMAGES.logos.logo.src,
+    card: "summary_large_image",
+  },
+};
 
 const Search = () => {
   return (
