288b99e40c34c3b697908aa20762b9d8f4ec58cf
Victor Ibironke <victoribironke2019@gmail.com>
2025-11-15 14:09:40 +0100

    removed geist from being used

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,9 +1,9 @@
-import { Domine, Geist } from "next/font/google";
+import { Domine } from "next/font/google";
 
-export const geist = Geist({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
+// export const geist = Geist({
+//   subsets: ["latin"],
+//   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+// });
 
 export const domine = Domine({
   subsets: ["latin"],
