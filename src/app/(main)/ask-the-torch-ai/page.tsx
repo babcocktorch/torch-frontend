a2eaa210c8119055bddb2f9e45b14bb8d3d21994
Victor Ibironke <victoribironke2019@gmail.com>
2026-01-05 21:38:48 +0100

    add: create Ask the Torch AI page with metadata and ComingSoon component

---
+++ src/app/(main)/ask-the-torch-ai/page.tsx
@@ -0,0 +1,43 @@
+import ComingSoon from "@/components/general/coming-soon";
+import { BASE_URL, IMAGES, PAGES } from "@/lib/constants";
+import { Metadata } from "next";
+
+export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "Ask the Torch AI | The Babcock Torch",
+  metadataBase: new URL(BASE_URL),
+  description:
+    "Ask the Torch AI is a tool that allows you to ask questions about the Babcock Torch.",
+  alternates: {
+    canonical: BASE_URL,
+  },
+  openGraph: {
+    images: BASE_URL + IMAGES.logos.logo.src,
+    url: BASE_URL + PAGES.ask_the_torch_ai,
+    title: "Ask the Torch AI | The Babcock Torch",
+    description:
+      "Ask the Torch AI is a tool that allows you to ask questions about the Babcock Torch.",
+    type: "website",
+    siteName: "Ask the Torch AI | The Babcock Torch",
+  },
+  twitter: {
+    title: "Ask the Torch AI | The Babcock Torch",
+    description:
+      "Ask the Torch AI is a tool that allows you to ask questions about the Babcock Torch.",
+    images: BASE_URL + IMAGES.logos.logo.src,
+    card: "summary_large_image",
+  },
+};
+
+const AskTheTorchAIPage = () => {
+  return (
+    <ComingSoon
+      title="Ask the Torch AI"
+      description="Ask the Torch AI is a tool that allows you to ask questions about the Babcock Torch."
+      showReturnButton={true}
+    />
+  );
+};
+
+export default AskTheTorchAIPage;
