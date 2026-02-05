ae4be8d9befe2b8c9ed94a051bb2c075d88158ae
Victor Ibironke <victoribironke2019@gmail.com>
2026-02-05 07:47:00 +0100

    chore: remove deployment configuration from Sanity CLI.

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,12 +6,4 @@ export default defineCliConfig({
     projectId: CREDENTIALS.sanity_project_id,
     dataset: CREDENTIALS.sanity_dataset,
   },
-  deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/cli#auto-updates
-     */
-    autoUpdates: true,
-    appId: "hy26z3f4aj5miqd2u8i1yw9v",
-  },
 });
