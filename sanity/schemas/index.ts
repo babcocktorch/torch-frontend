9200e85c662533b809324d7c2f66258aaa435a27
Victor Ibironke <victoribironke2019@gmail.com>
2026-02-08 06:00:33 +0100

    feat: add mastheadGuard schema type

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -3,5 +3,13 @@ import category from "./category";
 import post from "./post";
 import author from "./author";
 import masthead from "./masthead";
+import mastheadGuard from "./mastheadGuard";
 
-export const schemaTypes = [post, author, category, blockContent, masthead];
+export const schemaTypes = [
+  post,
+  author,
+  category,
+  blockContent,
+  masthead,
+  mastheadGuard,
+];
