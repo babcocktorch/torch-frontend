7a9d7180ea9d0a11bffb508f9cf8f5a45f4c0929
Victor Ibironke <victoribironke2019@gmail.com>
2026-03-09 08:05:40 +0100

    feat: update eslint.config.mjs to use native flat config imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,24 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off", // Disable the rule
-      "react-hooks/exhaustive-deps": "off", // Disable the exhaustive-deps rule
-      "@next/next/no-img-element": "off", // Disable the no-img-element rule
-    },
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
+  ignores: [
+    "node_modules/**",
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off", // Disable the rule
+    "react-hooks/exhaustive-deps": "off", // Disable the exhaustive-deps rule
+    "@next/next/no-img-element": "off", // Disable the no-img-element rule
   },
-];
+}];
 
 export default eslintConfig;
