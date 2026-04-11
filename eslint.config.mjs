00ca1763cf6c1cee51f80b1c9130d8614f0b2a88
Victor Ibironke <victoribironke2019@gmail.com>
2026-04-11 07:29:33 +0100

    style: format eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,19 +6,23 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
-  ignores: [
-    "node_modules/**",
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ],
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off", // Disable the rule
-    "react-hooks/exhaustive-deps": "off", // Disable the exhaustive-deps rule
-    "@next/next/no-img-element": "off", // Disable the no-img-element rule
+const eslintConfig = [
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // Disable the rule
+      "react-hooks/exhaustive-deps": "off", // Disable the exhaustive-deps rule
+      "@next/next/no-img-element": "off", // Disable the no-img-element rule
+    },
   },
-}];
+];
 
 export default eslintConfig;
