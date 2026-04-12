39b8addc36f19e108334da1e2b5ba4b1b34e8d31
Victor Ibironke <victoribironke2019@gmail.com>
2026-04-12 09:11:01 +0100

    fix: remove unused index variable in most-read component

--- src/components/opinions/most-read.tsx
+++ src/components/opinions/most-read.tsx
@@ -30,7 +30,7 @@ export default function MostRead({ opinions }: { opinions: PostType[] }) {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {popularPosts.map((opinion, idx) => {
+        {popularPosts.map((opinion) => {
           const readCount = opinion.readCount || 0;
 
           return (
