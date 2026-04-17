399a392d6d7a9e6c9592f2cb01153c7568e56f88
Victor Ibironke <victoribironke2019@gmail.com>
2026-04-17 17:18:20 +0100

    feat: update title font to Georgia in MostRead component

--- src/components/general/most-read.tsx
+++ src/components/general/most-read.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { PostType } from "@/lib/types";
 import { PAGES } from "@/lib/constants";
-import { domine } from "@/lib/fonts";
+import { georgia } from "@/lib/fonts";
 
 interface MostReadProps {
   posts: PostType[];
@@ -33,7 +33,7 @@ const MostRead = ({ posts, limit = 5 }: MostReadProps) => {
                 <h4
                   className={cn(
                     "text-sm lg:text-base font-medium group-hover:text-gold transition-colors leading-snug",
-                    domine.className
+                    georgia.className,
                   )}
                 >
                   {post.title}
