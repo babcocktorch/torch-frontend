3fe28a2301f996a6b71850ee277323a6bbd8452e
Victor Ibironke <victoribironke2019@gmail.com>
2026-04-17 17:19:14 +0100

    feat: replace Domine font with Georgia

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,11 +1,3 @@
-import { Domine } from "next/font/google";
-
-// export const geist = Geist({
-//   subsets: ["latin"],
-//   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-// });
-
-export const domine = Domine({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-});
+export const georgia = {
+  className: "font-georgia",
+};
