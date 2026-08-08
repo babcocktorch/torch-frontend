bbae5ed372d929da7933e28769c7a3dba291c9f9
Victor Ibironke <victoribironke2019@gmail.com>
2026-08-08 16:59:27 +0100

    feat: increase image quality to 100 for main images in MostControversial component

--- src/components/opinions/most-controversial.tsx
+++ src/components/opinions/most-controversial.tsx
@@ -49,11 +49,11 @@ export default function MostControversial({
               >
                 {opinion.mainImage ? (
                   <Image
-                    quality={90}
+                    quality={100}
                     src={urlFor(opinion.mainImage)
                       .width(600)
                       .height(400)
-                      .quality(90)
+                      .quality(100)
                       .fit("crop")
                       .url()}
                     alt={opinion.title}
