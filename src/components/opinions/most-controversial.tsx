4fc63c184ec8d2483f312ce38eff90f75d1cd1fb
Victor Ibironke <victoribironke2019@gmail.com>
2026-08-08 12:30:51 +0100

    feat: add quality option to MostControversial image rendering

--- src/components/opinions/most-controversial.tsx
+++ src/components/opinions/most-controversial.tsx
@@ -49,6 +49,7 @@ export default function MostControversial({
               >
                 {opinion.mainImage ? (
                   <Image
+                    quality={90}
                     src={urlFor(opinion.mainImage)
                       .width(600)
                       .height(400)
